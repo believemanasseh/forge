1385447a987cdf3aa5d6e8f7b9784b671d0b24ca
Manasseh <believemanasseh@gmail.com>
2025-04-08 12:44:17 +0100

    feat(agent): use mailbox

--- agent/src/config.py
+++ agent/src/config.py
@@ -6,10 +6,10 @@ from pydantic_settings import BaseSettings
 
 
 class Configuration(BaseSettings):
-    NAME: str = "Doki"
-    PORT: str = "5000"
-    ENDPOINT: str = "https://forge-api.daimones.xyz/submit"
-    MAILBOX: bool = False
+    NAME: Optional[str] = "Doki"
+    PORT: Optional[str] = None
+    ENDPOINT: Optional[str] = None
+    MAILBOX: Optional[bool] = False
     SEED: Optional[str] = None
     LLM_API_KEY: Optional[str] = None
     LLM_API_URL: Optional[str] = None
